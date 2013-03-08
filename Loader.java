5a775f7871c7e464c07aa3acd4df4e1af2b84e36
aerospark <lukefay12@gmail.com>
2013-03-08 00:46:12 -0500

    Initial commit

---
+++ Loader.java
@@ -0,0 +1,20 @@
+/*
+ *                    BITIVERSE SOFTWARE DEVELOPMENT LICENSE EXTENSION
+ *    Bitiverse is open-source software, which is largely licensed under the LGPL v3.
+ *      This software is developed and maintained by the Bitiverse Development Squad.
+ * 
+ *    This project makes use of the Lightweight Java Game Library (LWJGL) Project.
+ * 
+ *    Some code in this project is in the public domain.
+ */
+
+
+/**
+ *
+ * @author luke
+ */
+public class Loader {
+    public static void main(String[] str){
+    
+    }
+}
---
+++ com/bitiverse/resources/Resources.java
@@ -0,0 +1,22 @@
+/*
+ *                    BITIVERSE SOFTWARE DEVELOPMENT LICENSE EXTENSION
+ *    Bitiverse is open-source software, which is largely licensed under the LGPL v3.
+ *      This software is developed and maintained by the Bitiverse Development Squad.
+ * 
+ *    This project makes use of the Lightweight Java Game Library (LWJGL) Project.
+ * 
+ *    Some code in this project is in the public domain.
+ */
+package com.bitiverse.resources;
+
+/**
+ *
+ * @author luke
+ */
+public class Resources {
+    public static final int TYPE_ELECTRICITY = 1;
+    public static final int TYPE_WATER = 2;
+    public static final int TYPE_WASTE = 3;
+    public static final int TYPE_GAS = 4; // not gasoline (ick)
+    public static final int TYPE_COMMUNICATIONS = 5;
+}
